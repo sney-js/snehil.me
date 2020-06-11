26d963260586c3e17831a5f4db224267b59ce842
Snehil Bhushan <Snehil.Bhushan@akqa.com>
2020-06-11 16:34:53 +0100

    layout and language

--- src/containers/Layout/Layout.tsx
+++ src/containers/Layout/Layout.tsx
@@ -1,47 +1,15 @@
 import React from 'react';
-import { getUrl } from '../../utils/RespImage';
 import { makeClass, setCSSVar } from 'utils/Helpers';
 import HeaderContainer from 'containers/HeaderContainer';
 import FooterContainer from 'containers/FooterContainer';
-import { Helmet } from 'react-helmet';
-import Container from '../../components/Container';
-import CookieBanner from '../../components/CookieBanner';
-import CookieBannerContainer from '../CookieBannerContainer';
-
-const styles = require('./layout.module.scss');
-
-type MetaDataStructure = {
-  title: string;
-  description?: string;
-  keywords?: string[];
-  image?: string;
-};
+import Container from 'components/Container';
+import CookieBannerContainer from 'containers/CookieBannerContainer';
 
 type LayoutProps = {
   locale?: string;
   globalLoader?: boolean;
   children?: any;
   theme?: 'dark' | 'light';
-};
-
-export const MetaData = (props: MetaDataStructure) => {
-  return (
-    <Helmet>
-      <title>{props?.title}</title>
-      {props?.description ? (
-        <meta name='description' content={props.description} />
-      ) : null}
-      {props?.keywords ? (
-        <meta name='keywords' content={props.keywords?.join(',')} />
-      ) : null}
-      {props?.image ? (
-        <meta
-          property='og:image'
-          content={props.image && getUrl(props.image).replace('//', 'https://')}
-        />
-      ) : null}
-    </Helmet>
-  );
 };
 
 const globalInitialVals = {};
@@ -51,14 +19,12 @@ export const GlobalContext = React.createContext(globalInitialVals);
 function Layout(props: LayoutProps) {
   const globalState = {};
 
-  console.log(props.children, 'props.children');
-
   return (
-    <div className={makeClass([styles.layout])}>
+    <div className={makeClass(['d-layout'])}>
       <GlobalContext.Provider value={globalState}>
         <HeaderContainer />
 
-        <div className={makeClass([styles.content])}>
+        <div className={makeClass(['d-content'])}>
           {props.children ? (
             <main>{props.children}</main>
           ) : (
@@ -71,7 +37,7 @@ function Layout(props: LayoutProps) {
           )}
         </div>
 
-        <div className={styles.cookieBanner}>
+        <div className={'d-cookieBanner'}>
           <CookieBannerContainer />
         </div>
 
