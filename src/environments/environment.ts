c8d6880b8b9ffab14b0d329d4b6fac068460d0cb
Snehil Bhushan <Snehil.Bhushan@akqa.com>
2020-06-11 18:50:49 +0100

    env cleanup.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: false,
   localStorageKey: null,
-  apiUrl: '',
-  googleTag: null,
-  preview: false,
-  contentful: {
-    space: '',
-    accessToken: '',
-    environment: ''
-  }
 };
