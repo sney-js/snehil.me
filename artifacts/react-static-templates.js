70ef83780da6d29b9fb64b516ca4b0d4044ee79a
Snehil Bhushan <Snehil.Bhushan@akqa.com>
2020-06-12 22:01:04 +0100

    wip no luck.

--- artifacts/react-static-templates.js
+++ artifacts/react-static-templates.js
@@ -4,9 +4,9 @@
 // Template Map
 export default {
   '__react_static_root__/node_modules/react-static/lib/browser/components/Default404': require('__react_static_root__/node_modules/react-static/lib/browser/components/Default404').default,
-'__react_static_root__/src/app/pages/Page_page': require('__react_static_root__/src/app/pages/Page_page').default,
-'__react_static_root__/src/app/pages/Page_article': require('__react_static_root__/src/app/pages/Page_article').default,
-'__react_static_root__/src/app/pages/Page_category': require('__react_static_root__/src/app/pages/Page_category').default
+'__react_static_root__/src/containers/page/Page_page': require('__react_static_root__/src/containers/page/Page_page').default,
+'__react_static_root__/src/containers/page/Page_article': require('__react_static_root__/src/containers/page/Page_article').default,
+'__react_static_root__/src/containers/page/Page_category': require('__react_static_root__/src/containers/page/Page_category').default
 }
 
 export const notFoundTemplate = '__react_static_root__/node_modules/react-static/lib/browser/components/Default404'
---
+++ src/containers/page/Page_article.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useRouteData } from "react-static";
+import "./page.scss";
+import Layout, { MetaData } from "../../containers/Layout";
+import { renderContentContainer } from "../../contentful/Renderer";
+import Flatted from "flatted";
+
+type PageProps = {
+    changeTheme?: Function;
+    changeLocale?: Function;
+    path?: string;
+    routeData?: any;
+};
+
+const Page_page = (props: PageProps) => {
+    let page, locale;
+
+    if (props.routeData) {
+        page = props.routeData.page;
+        locale = props.routeData.locale;
+    } else {
+        const routeData = useRouteData();
+        page = routeData.page;
+        page = Flatted.parse(page);
+        locale = routeData.locale;
+    }
+    let metaData = page.fields.metaData ? page.fields.metaData.fields : "";
+    return (
+        <Layout locale={locale}>
+            <MetaData {...metaData} />
+            {page.fields.content &&
+                page.fields.content.map((item, index) =>
+                    renderContentContainer({ item, key: index }),
+                )}
+        </Layout>
+    );
+};
+
+export default Page_page;
